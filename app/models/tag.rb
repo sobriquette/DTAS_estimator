2f598baa04eea84eda5af5d394ea416d64b0875e
sobriquette <11lin.m7@gmail.com>
2015-10-06 13:39:34 -0400

    reverting to old working code with .blank?

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,7 +8,7 @@ class Tag < ActiveRecord::Base
 
 
 	def self.est_time(tag_id)
-		if Tag.where("tags.name in (?) ", tag_id).any? && !(Tag.joins(:tasks).where("tags.name in (?) ", tag_id ).where("actual_time is NULL OR actual_time IN ('0','')"))
+		if Tag.where("tags.name in (?) ", tag_id).any? && !(Tag.joins(:tasks).where("tags.name in (?) ", tag_id ).where("actual_time").blank?)
   			avg = Tag.joins(:tasks).where("tags.name in (?) ", tag_id ).average("actual_time")
   		else
   			avg = 16
